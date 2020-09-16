d3fe491809051876ad24d78c7b6ceca44a0b0591
KIMBERLY LENSICO <KIMBERLYLENSICO@OUTLOOK.COM>
2020-09-15 21:15:31 -0700

    attempted charts... will work on it tomorrow

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+//  - - - - - - - LAB 11 BEGINS HERE
+
 'use strict'
 
 // keep track of which images have been displayed so none of them are shown on the next refresh
-BusMallItem.allItems = []; // Bussmall is going to be the name of a constructor. When we make the constructor, it'll put whatever is in the prototype INTO the array
+Item.allItems = []; // Bussmall is going to be the name of a constructor. When we make the constructor, it'll put whatever is in the prototype INTO the array
 var displayHistory = []; // keeps clicked history of the last displayed images in the last round
 
 // the following gives access to the <img> in index.html 
@@ -23,43 +25,43 @@ var maxRounds = 25; // total number of voting rounds
 // times clicked on
 // times viewed
 // gives name, source
-function BusMallItem(name, source) {
+function Item(name, source) {
     //constructor accepts name and source ONLY
     //has four properties, but only specifies what the first two have to be displayed
     this.name = name;
     this.source = source;
     this.viewed = 0; // starts with 0 bc never viewed 
     this.clicked = 0; // never been clicked on
-    BusMallItem.allItems.push(this) // spits out an object with four properties and pushes them into the entire instance of this.[EVERYTHING]
+    Item.allItems.push(this) // spits out an object with four properties and pushes them into the entire instance of this.[EVERYTHING]
 }
 
 // underneath is all the bus items for the images that are randomly selected 
-new BusMallItem('bag', './img/bag.jpg'); // creating a new busmall item with those specific parameters from your BusMall Constructor 
-new BusMallItem('banana', './img/banana.jpg');
-new BusMallItem('bathroom', './img/bathroom.jpg');
-new BusMallItem('boots', './img/boots.jpg');
-new BusMallItem('breakfast', './img/breakfast.jpg');
-new BusMallItem('bubblegum', './img/bubblegum.jpg');
-new BusMallItem('chair.jpg', './img/chair.jpg');
-new BusMallItem('cthulu', './img/cthulhu.jpg');
-new BusMallItem('dog-duck', './img/dog-duck.jpg')
-new BusMallItem('dragon', './img/dragon.jpg');
-new BusMallItem('pen', './img/pen.jpg');
-new BusMallItem('pet-sweep', './img/pet-sweep.jpg');
-new BusMallItem('scissors', './img/scissors.jpg')
-new BusMallItem('shark', './img/shark.jpg');
-new BusMallItem('sweep', './img/sweep.png');
-new BusMallItem('tauntaun', './img/tauntaun.jpg');
-new BusMallItem('unicorn', './img/unicorn.jpg');
-new BusMallItem('usb', './img/usb.gif');
-new BusMallItem('water-can', './img/water-can.jpg');
-new BusMallItem('wine-glass', './img/wine-glass.jpg')
+new Item('bag', './img/bag.jpg'); // creating a new busmall item with those specific parameters from your BusMall Constructor 
+new Item('banana', './img/banana.jpg');
+new Item('bathroom', './img/bathroom.jpg');
+new Item('boots', './img/boots.jpg');
+new Item('breakfast', './img/breakfast.jpg');
+new Item('bubblegum', './img/bubblegum.jpg');
+new Item('chair.jpg', './img/chair.jpg');
+new Item('cthulu', './img/cthulhu.jpg');
+new Item('dog-duck', './img/dog-duck.jpg')
+new Item('dragon', './img/dragon.jpg');
+new Item('pen', './img/pen.jpg');
+new Item('pet-sweep', './img/pet-sweep.jpg');
+new Item('scissors', './img/scissors.jpg')
+new Item('shark', './img/shark.jpg');
+new Item('sweep', './img/sweep.png');
+new Item('tauntaun', './img/tauntaun.jpg');
+new Item('unicorn', './img/unicorn.jpg');
+new Item('usb', './img/usb.gif');
+new Item('water-can', './img/water-can.jpg');
+new Item('wine-glass', './img/wine-glass.jpg')
 
 // creating a random item function to randomly generate the three images
 function randomBusMallItem() { // not uppercase R bc we are not constructing anything
     //random number?
     //calc value between min and max
-    var random = Math.floor(Math.random() * (BusMallItem.allItems.length)); // using math. random to generate a random number, and multiply bus mall times the amount of objects (the BusMallItems') in the array
+    var random = Math.floor(Math.random() * (Item.allItems.length)); // using math. random to generate a random number, and multiply bus mall times the amount of objects (the BusMallItems') in the array
     return random;
 }
 
@@ -68,26 +70,24 @@ function renderItems() {
     var renderItem = [leftID.src, centerID.src, rightID.src]; //RENDERING THE SOURCE OF THE ITEMS
     var tempItems = []; // CREATES THE ARRAY FOR THE OF THE ITEMS THAT ARE TO BE SHOWN
 
-    var generatedItem = BusMallItem.allItems[randomBusMallItem()].image;
+    var generatedItem = Item.allItems[randomBusMallItem()].image;
 
     while (!renderItem.includes[generatedItem] && tempItems.length < 3) { // only allows up to three images to be shown in from the array
-        generatedItem = BusMallItem.allItems[randomBusMallItem()].image;
+        generatedItem = Item.allItems[randomBusMallItem()].image;
         tempItems.push(generatedItem);
     }
     renderItems = tempItems; // eventually this will fill all our images
 
     // This code just makes sure the images are rendered 
-    leftID.src = BusMallItem.allItems[randomBusMallItem()].source; // the [html] 'leftID src' is located at [#]; it is 'source on the right because we're grabbing the source from the constructor. we are inside 'leftID', while we're inside of it, we're telling it what it is equal to  
-    centerID.src = BusMallItem.allItems[randomBusMallItem()].source; // the .source allows us to touch the src value. we're going inside our array and are touching the 'source' source of fthe array
-    rightID.src = BusMallItem.allItems[randomBusMallItem()].source;
+    leftID.src = Item.allItems[randomBusMallItem()].source; // the [html] 'leftID src' is located at [#]; it is 'source on the right because we're grabbing the source from the constructor. we are inside 'leftID', while we're inside of it, we're telling it what it is equal to  
+    centerID.src = Item.allItems[randomBusMallItem()].source; // the .source allows us to touch the src value. we're going inside our array and are touching the 'source' source of fthe array
+    rightID.src = Item.allItems[randomBusMallItem()].source;
     // we are looping through the array to get the random images
 }
 
-renderItems();
-
 var usedIDs = [];
 var usedimg = 0;
-if (usedIDs.length != BusMallItem.allItems.length) {
+if (usedIDs.length != Item.allItems.length) {
     do { // <= for when no image has been used yet
         usedimg = Math.floor(Math.random() * randomBusMallItem.length);
     } while (usedIDs.indexOf(usedimg) > -1);
@@ -101,10 +101,30 @@ if (usedIDs.length != BusMallItem.allItems.length) {
 console.log(randomBusMallItem()); // console is firing the function. we get back whatever is returned in the array
 
 // // keep track of votes per image
-// function clickCount(event) {
-//     vote += 1;
-//     if (vote > 24) { // 24 because first voting starts at 0. when it hits '25' total votes then it's over
-//     }
-// }
+function handleVote(event) {
 
-//     //
+    var clickedItems = event.target
+    var itemId = clickedItems.src;
+
+    for (var i = 0; i < Item.allItems.length; i++) { // vote count is zero and encompasses all items to vote on within the BM array
+        if (itemId.includes(Item[i].source)) {
+            Item.allItems[i].clicks += 1;
+            console.log(Item.allItems[i].name, Item.allItems[i].clicks);
+        }
+    }
+
+    renderItems();
+}
+
+leftID.addEventListener('click', handleVote);
+centerID.addEventListener('click', handleVote);
+rightID.addEventListener('click', handleVote);
+
+renderItems();
+
+
+//  - - - - LAB 12 BEGINS HERE - - - - 
+
+// going into the chart element and selecting what goes inside of it 
+var voters = document.getElementById('voters').getContext('2d');
+new Chart(voters).line(voteCount);
